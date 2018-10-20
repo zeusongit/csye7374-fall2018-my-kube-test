b3d5fa424eab3b7ee51b325567d0874dfeb21a4c
Ashish <aggarwal.ash@husky.neu.edu>
2018-10-20 00:46:09 -0400

    update for a2

--- redis/setup.sh
+++ redis/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-kubectl create -f templates/redis-master.yaml & sleep 60;
-kubectl create -f templates/nodeport-service.yaml
+kubectl create -f templates/master-replica.yaml
+kubectl create -f templates/master-service.yaml
 kubectl create -f templates/redis-sentinel-service.yaml
-kubectl create -f templates/redis-controller.yaml
-kubectl create -f templates/redis-sentinel-controller.yaml
-
-kubectl delete pods redis-master
+kubectl create -f templates/nodeport-service.yaml
+kubectl create -f templates/slave-replica.yaml
+kubectl create -f templates/slave-service.yaml
