4a7a0352af6881e6bfbf5595038992c27caf3398
Ashish <aggarwal.ash@husky.neu.edu>
2018-10-20 00:58:43 -0400

    update for a2

--- redis/k8setup.sh
+++ redis/k8setup.sh
@@ -3,4 +3,11 @@
 export NAME=csye6225-spring2018-aggarwalash.me
 export KOPS_STATE_STORE=s3://csye6225-spring2018-aggarwalash.me
 
-kops create cluster --zones us-east-1a --node-size t2.micro --master-size t2.micro $NAME --node-count 3 --yes
+kops create cluster \
+--zones us-east-1a \
+--node-size t2.micro \
+--master-size t2.micro \
+--node-count 3 \
+--node-security-groups sg-0cac0878726527557\
+--master-security-groups sg-0cac0878726527557\
+$NAME --yes
