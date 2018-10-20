a9617be6aff270a268f4f5729f3cd91d25ac1543
Ashish <aggarwal.ash@husky.neu.edu>
2018-10-20 01:53:22 -0400

    update for a2

--- redis/teardown.sh
+++ redis/teardown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl delete -f templates/master-replica.yaml
 kubectl delete -f templates/master-service.yaml
+kubectl delete -f templates/master-replica.yaml
 kubectl delete -f templates/redis-sentinel-service.yaml
 kubectl delete -f templates/slave-replica.yaml
 kubectl delete -f templates/slave-service.yaml
